c1f9ebb20e3b90c4dc40da5dbbe8d0fc9fabc318
Alejandro <alexsanchezarbeit@outlook.com>
2018-05-03 17:02:45 +0200

    Commit inicial

---
+++ app.js
@@ -0,0 +1,25 @@
+const {crearArchivo} = require('./multiplicar/multiplicar')
+const argv = require('yargs')
+    .command('listar','Imprime en consola la tabla de multiplicar',{
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .help()
+    .argv;
+
+//console.log(process.argv)
+let argv2 = process.argv;
+
+console.log('Limite', argv.limite);
+//let parametro = argv[2];
+//let base = parametro.split('=')[1];
+
+//crearArchivo(base)
+  //  .then(archivo => console.log(`Archivo creado: ${archivo}`))
+    //.catch(e => console.log(e));
